dcb8520a811f27fd855edbed82429d1c8ced7c0b
hadiazb <hadiazb@unal.edu.co>
2020-01-29 14:28:08 -0500

    Agregando stylos

--- js/js.js
+++ js/js.js
@@ -4,15 +4,34 @@ var tabla = [
     "nombre": null ,
     "edad"  : null },
 ];
+var tablaMayores = [
+  {
+    "nombre": null ,
+    "edad"  : null },
+];
 
 function cargarEventos(){
-  document.getElementById("botonRegistrar").addEventListener( "click", nuevoEstudiante, false);
-  // document.getElementById("botonLista").addEventListener( "click", mostrarTabla, false);
+  document.getElementById("botonRegistrar").addEventListener( "click", nuevaPersona, false);
+  document.getElementById("botonLista").addEventListener( "click", mostrarTabla, false);
+  // document.getElementById("botonLista").addEventListener( "click", mostrarMayores, false);
 }
 
-function nuevoEstudiante(){
+function nuevaPersona(){
   var introNombre = document.getElementById("txtNombre").value;
   var introEdad   = parseFloat(document.getElementById("txtEdad").value);
-  var nuevoEstudiante = { "nombre": introNombre, "nota": introEdad };
-  tabla.push(nuevoEstudiante);
+  var nuevaPersona = { "nombre": introNombre, "edad": introEdad };
+  tabla.push(nuevaPersona);
+  var limpiar = document.getElementById('txtNombre').value
+  limpiar.innerHTML = ""
+
+
+}
+
+function mostrarTabla(){
+  var cuerpoTabla = document.getElementById("datos-tabla");
+  var tablaLlena = "";
+  for (var i = 1; i < tabla.length; i++) {
+    tablaLlena += "<tr><td>" + tabla[i].nombre + "</td><td>" + tabla[i].edad + "</td></tr>" ;
+  }
+  cuerpoTabla.innerHTML = tablaLlena;
 }
