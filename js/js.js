f6bf5e02b2f362f2cd63d5a9306170b565c737f5
hadiazb <hadiazb@unal.edu.co>
2020-01-31 13:32:21 -0500

    Lista de mayores de edad

--- js/js.js
+++ js/js.js
@@ -14,7 +14,7 @@ function cargarEventos(){
   document.getElementById("botonRegistrar").addEventListener( "click", nuevaPersona, false);
   document.getElementById("botonRegistrar").addEventListener( "click", limpiar, false);
   document.getElementById("botonLista").addEventListener( "click", mostrarTabla, false);
-  // document.getElementById("botonLista").addEventListener( "click", mostrarMayores, false);
+  document.getElementById("botonMayoresDeEdad").addEventListener( "click", mostrarMayores, false);
 }
 
 function nuevaPersona(){
@@ -35,4 +35,15 @@ function mostrarTabla(){
 function limpiar() {
     document.getElementById("txtNombre").value = "";
     document.getElementById("txtEdad").value = "";
+}
+function mostrarMayores(){
+  var cuerpoTabla = document.getElementById("data-table");
+  var mayorDeEdad = 18;
+  var tablaLlena = "";
+  for (var i = 1; i < tabla.length; i++) {
+    if (tabla[i].edad >= mayorDeEdad) {
+      tablaLlena += "<tr><td>" + tabla[i].nombre + "</td><td>" + tabla[i].edad + "</td></tr>" ;
+    }
+    cuerpoTabla.innerHTML = tablaLlena;
+  }
 }
